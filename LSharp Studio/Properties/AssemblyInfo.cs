e2968bc7eb91604d43a7d11b63c42e047f93d08f
mlnlover11 <elijah.frederickson@gmail.com>
2012-03-01 14:25:49 -0900

    updates

--- LSharp Studio/Properties/AssemblyInfo.cs
+++ LSharp Studio/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LSharp Studio")]
-[assembly: AssemblyCopyright("Copyright (C) mlnlover11 Productions 2011")]
+[assembly: AssemblyCopyright("Copyright (C) mlnlover11 Productions 2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- LSharp Studio/SplashScreen.Designer.cs
+++ LSharp Studio/SplashScreen.Designer.cs
@@ -28,68 +28,68 @@ namespace LSharp.Studio
         /// </summary>
         private void InitializeComponent()
         {
-        	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
-        	this.lblCopyrights = new System.Windows.Forms.Label();
-        	this.lblLoadingMessage = new System.Windows.Forms.Label();
-        	this.label1 = new System.Windows.Forms.Label();
-        	this.SuspendLayout();
-        	// 
-        	// lblCopyrights
-        	// 
-        	this.lblCopyrights.AutoSize = true;
-        	this.lblCopyrights.BackColor = System.Drawing.Color.Transparent;
-        	this.lblCopyrights.ForeColor = System.Drawing.Color.Black;
-        	this.lblCopyrights.Location = new System.Drawing.Point(2, 85);
-        	this.lblCopyrights.Name = "lblCopyrights";
-        	this.lblCopyrights.Size = new System.Drawing.Size(302, 26);
-        	this.lblCopyrights.TabIndex = 0;
-        	this.lblCopyrights.Text = "Copyright © 2005-2008 Rob Blackwell && Active Web Solutions\r\nCopyright © 2011 mln" +
-        	"lover11 Productions";
-        	this.lblCopyrights.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        	// 
-        	// lblLoadingMessage
-        	// 
-        	this.lblLoadingMessage.AutoSize = true;
-        	this.lblLoadingMessage.BackColor = System.Drawing.Color.Transparent;
-        	this.lblLoadingMessage.Location = new System.Drawing.Point(247, 127);
-        	this.lblLoadingMessage.Name = "lblLoadingMessage";
-        	this.lblLoadingMessage.Size = new System.Drawing.Size(57, 13);
-        	this.lblLoadingMessage.TabIndex = 1;
-        	this.lblLoadingMessage.Text = "Loading ...";
-        	this.lblLoadingMessage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-        	// 
-        	// label1
-        	// 
-        	this.label1.BackColor = System.Drawing.Color.Transparent;
-        	this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        	this.label1.Location = new System.Drawing.Point(83, 9);
-        	this.label1.Name = "label1";
-        	this.label1.Size = new System.Drawing.Size(122, 34);
-        	this.label1.TabIndex = 3;
-        	this.label1.Text = "Studio";
-        	// 
-        	// SplashScreen
-        	// 
-        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        	this.BackColor = System.Drawing.Color.White;
-        	this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-        	this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-        	this.ClientSize = new System.Drawing.Size(303, 149);
-        	this.Controls.Add(this.label1);
-        	this.Controls.Add(this.lblLoadingMessage);
-        	this.Controls.Add(this.lblCopyrights);
-        	this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
-        	this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-        	this.MaximizeBox = false;
-        	this.MinimizeBox = false;
-        	this.Name = "SplashScreen";
-        	this.ShowIcon = false;
-        	this.ShowInTaskbar = false;
-        	this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-        	this.TopMost = true;
-        	this.ResumeLayout(false);
-        	this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.lblCopyrights = new System.Windows.Forms.Label();
+            this.lblLoadingMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCopyrights
+            // 
+            this.lblCopyrights.AutoSize = true;
+            this.lblCopyrights.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyrights.ForeColor = System.Drawing.Color.Black;
+            this.lblCopyrights.Location = new System.Drawing.Point(2, 85);
+            this.lblCopyrights.Name = "lblCopyrights";
+            this.lblCopyrights.Size = new System.Drawing.Size(302, 26);
+            this.lblCopyrights.TabIndex = 0;
+            this.lblCopyrights.Text = "Copyright © 2005-2008 Rob Blackwell && Active Web Solutions\r\nCopyright © 2011-201" +
+            "2 mlnlover11 Productions";
+            this.lblCopyrights.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLoadingMessage
+            // 
+            this.lblLoadingMessage.AutoSize = true;
+            this.lblLoadingMessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblLoadingMessage.Location = new System.Drawing.Point(247, 127);
+            this.lblLoadingMessage.Name = "lblLoadingMessage";
+            this.lblLoadingMessage.Size = new System.Drawing.Size(57, 13);
+            this.lblLoadingMessage.TabIndex = 1;
+            this.lblLoadingMessage.Text = "Loading ...";
+            this.lblLoadingMessage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Studio";
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(303, 149);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLoadingMessage);
+            this.Controls.Add(this.lblCopyrights);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashScreen";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
         private System.Windows.Forms.Label label1;
 
